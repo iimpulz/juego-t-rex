e134df62f3d78403bf37942456789081b0ae4642
iimpulz <96321241+iimpulz@users.noreply.github.com>
2022-04-15 17:47:56 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,18 +33,18 @@ function preload(){
 }
 
 function setup(){
-  createCanvas(600,200);
+  createCanvas(windowWidth, windowHeight);
   
   //crear sprite de Trex
-  trex = createSprite(50,160,20,50);
+  trex = createSprite(50,height/2,20,50);
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided",trex_collider);
   edges = createEdgeSprites();
-  gameover = createSprite(300,100);
+  gameover = createSprite(width/2,height/2-100);
   gameover.addImage(gameoverImg);
   gameover.scale=0.5;
   gameover.visible=false;
-  restart = createSprite(300,140);
+  restart = createSprite(width/2,height/2-50);
   restart.addImage(restartImg);
   restart.scale=0.5;
   restart.visible=false;
@@ -53,9 +53,9 @@ function setup(){
   trex.scale = 0.5;
   trex.x = 50;
 
-  ground = createSprite(200,180,400,20);
+  ground = createSprite(width/2,height-20,400,20);
   ground.addImage("ground", groundImg);
-  invisibleground = createSprite(200,190,400,10);
+  invisibleground = createSprite(width/2,height-10,width,10);
   invisibleground.visible = false;
   cloudsGroup=createGroup();
   cactusGroup=createGroup();
@@ -79,9 +79,15 @@ function draw(){
       ground.x = ground.width/2;
     }
     //hacer que el Trex salte al presionar la barra espaciadora
-    if(keyDown("space") && trex.y > 150){
+    if(keyDown("space") && trex.y > height-80){
       trex.velocityY = -10; 
       jump.play();
+    }
+    
+    if(touches.length>0 && trex.y > height-80){
+      trex.velocityY = -10; 
+      jump.play();
+      touches=[];
     }
   
     trex.velocityY = trex.velocityY + 0.5;
@@ -105,8 +111,9 @@ function draw(){
     gameover.visible=true;
     restart.visible=true;
     trex.velocityY = 0;
-    if(mousePressedOver(restart)){
+    if(mousePressedOver(restart) || toches.length>0){
       reset();
+      touches=[];
     }
   }
   
@@ -122,20 +129,20 @@ function draw(){
 
 function clouds(){
   if(frameCount%60===0){
-    cloud = createSprite(600,100,40,10);
+    cloud = createSprite(width+30,100,40,10);
     cloud.addImage("cloud",cloudImg);
     cloud.velocityX =-3;
-    cloud.y = Math.round(random(10,100));
+    cloud.y = Math.round(random(10,height-100));
     cloud.depth = trex.depth;
     trex.depth+=1;
-    cloud.lifetime = 220;
+    cloud.lifetime = 400;
     cloudsGroup.add(cloud);
   }
 }
 
 function obstacle(){
   if(frameCount%60===0){
-    cactus=createSprite(600,165,10,40);
+    cactus=createSprite(width+30,height-40,10,40);
     cactus.velocityX= -(6+score/100);;
     
     var ran=Math.round(random(1,6));
@@ -163,7 +170,7 @@ function obstacle(){
         break;
     }
     cactus.scale = 0.5;
-    cactus.lifetime = 210;
+    cactus.lifetime = 400;
     cactusGroup.add(cactus);
   }
 }
